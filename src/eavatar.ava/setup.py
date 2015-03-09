1f82081b7162859849d9d7ecc00b1ba79b40cabf
sampot <sam@eavatar.com>
2015-03-09 09:35:01 +0800

    check in

--- src/eavatar.ava/setup.py
+++ src/eavatar.ava/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="eavatar.ava",
+    name="ava",
     version="0.1.0",
     description="EAvatar Ava - A versatile agent.",
     # package_dir={'': ''},
