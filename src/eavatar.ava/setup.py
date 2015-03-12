74ae65a947fece70ccde4b1866d0cb7d9253c484
sampot <sam@eavatar.com>
2015-03-12 20:58:31 +0800

    remove sample extension to another repository

--- src/eavatar.ava/setup.py
+++ src/eavatar.ava/setup.py
@@ -4,13 +4,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="ava",
-    version="0.1.5",
+    version="0.1.6",
     description="EAvatar Ava - A versatile agent.",
     # package_dir={'': ''},
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
 
-    # install_requires = ['setuptools'],
+    install_requires=['base58>=0.2.1',
+                      'bottle>=0.12.8',
+                      'click==3.3',
+                      'gevent==1.0.1',
+                      'libnacl==1.4.0',
+                      'lmdb==0.84',
+                      'pycrypto==2.6.1',
+                      'PyDispatcher==2.0.5',
+                      'PyYAML==3.11',
+                      'pyscrypt==1.6.2'],
     test_suite='nose.collector',
     zip_safe=False,
 
