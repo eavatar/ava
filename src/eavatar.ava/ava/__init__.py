2222b1ca89915cc64df5c8d07c70363d3d4e4bf4
sampot <sam@eavatar.com>
2015-04-01 10:33:05 +0800

    use epoc time as micro version

--- src/eavatar.ava/ava/__init__.py
+++ src/eavatar.ava/ava/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import time
+
 VERSION_MAJOR = 0
 VERSION_MINOR = 1
-VERSION_MICRO = 15
+VERSION_MICRO = time.time()
 
 __version__ = '%d.%d.%d' % (VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO)
 VERSION_STRING = __version__
